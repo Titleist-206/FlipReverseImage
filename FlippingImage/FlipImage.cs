c5bdd05405922502c3ffba6f5eabaf4cf99d6cb6
Andrew Lee <andrewilee@live.com>
2021-03-13 11:40:22 -0800

    Add project files.

---
+++ FlippingImage/FlipImage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlippingImage
+{
+    class FlipImage
+    {
+        public int[][] FlipAndInvertImage(int[][] matrix)
+        {
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                Array.Reverse(matrix[i]);  //first reverse (flip) each row in the matrix
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j]++;
+                    matrix[i][j] %= 2;  //Then invert each value
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
+
+
+
+
---
+++ FlippingImage/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlippingImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var flipImage = new FlipImage();  //Instantiate our class
+
+            int[][] matrix = new int[3][];  //Create a jagged array, populate
+            matrix[0] = new int[] { 1, 1, 0 };
+            matrix[1] = new int[] { 1, 0, 1 };
+            matrix[2] = new int[] { 0, 0, 0 };
+
+            flipImage.FlipAndInvertImage(matrix); 
+        }
+    }
+}
+
+
+
+
+//Input: image = [[1, 1, 0],[1,0,1],[0,0,0]]
+//Output:[[1,0,0],[0,1,0],[1,1,1]]
+//Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+//Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
